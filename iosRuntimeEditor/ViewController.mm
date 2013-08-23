5cee8bdd2ac02889dd9fb22184d10777be3febb9
lg <lg@lgs-Mac.local>
2013-08-24 02:05:54 +0800

    first step

--- iosRuntimeEditor/ViewController.mm
+++ iosRuntimeEditor/ViewController.mm
@@ -7,6 +7,103 @@
 //
 
 #import "ViewController.h"
+#include <list>
+
+@class PointContext;
+
+CGRect rectInflate(CGPoint pt, CGFloat edge)
+{
+    return CGRectMake(pt.x - edge/2, pt.y - edge/2, edge, edge);
+}
+#define LINEMARGIN 4
+
+
+enum EPtState {
+    eInside = 0x0000,
+    
+    eInCornerTL = 0x0001,
+    eInCornerTR = 0x0002,
+    eInCornerRB = 0x0004 ,
+    eInCornerBL = 0x0010,
+    
+    eInEdgeT = 0x0010,
+    eInEdgeL = 0x0020,
+    eInEdgeB = 0x0040,
+    eInEdgeR = 0x0100,
+    
+    eInCorner = eInCornerTL | eInCornerTR | eInCornerRB | eInCornerBL,
+    eInEdge = eInEdgeT | eInEdgeL | eInEdgeB | eInEdgeR,
+};
+
+EPtState ptState(CGPoint pt, CGRect frame)
+{
+//    if (rectInflate() ) {
+//
+//    }
+    
+    return  eInside;
+}
+
+
+bool isInCorner(EPtState e)
+{
+    return  (e | eInCorner) == eInCorner;
+}
+
+bool isInLine(EPtState e)
+{
+    return (e | eInEdge) == eInEdge;
+}
+
+
+
+@interface PointContext : NSObject
+@property (nonatomic) UIView *view;
+@property (nonatomic) enum EPtState ptCtx;
+@end
+
+@implementation PointContext
+@synthesize view, ptCtx;
+@end
+
+
+
+
+//find the deepest view
+//priority corner > line = block
+PointContext* hitTest(CGPoint pt,  UIView* root)
+{
+    PointContext * ctx = [[PointContext alloc] init];
+    ctx.view = root;
+    ctx.ptCtx = eInside;
+    std::list<UIView*> q;
+    q.push_front(root);
+    
+    while (!q.empty()) {
+        UIView *top = q.back();
+        q.pop_back();
+        
+        for(UIView *childView in top.subviews) {
+            CGPoint pt = [childView convertPoint: pt fromView:root];
+            enum EPtState state = ptState(pt, childView.frame);
+            if (isInCorner( state )) {
+                ctx.ptCtx = state;
+                ctx.view = childView;
+            }
+            else if (isInLine(state) && !isInCorner(ctx.ptCtx)) {
+                ctx.ptCtx = state;
+                ctx.view = childView;
+            }
+            else if ( state == eInside && !isInCorner(ctx.ptCtx)) {
+                ctx.ptCtx = state;
+                ctx.view = childView;
+            }
+            q.push_front(childView);
+        }
+    }
+    return ctx;
+}
+
 
 @interface ViewController ()
 
@@ -25,107 +122,11 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-#define LINEMARGIN 4
 
 
 
-enum EPtState
-{
-    inside=0x0000,
-    
-    topC=0x0010,
-    leftC=0x0020,
-    bottomC=0x0040,
-    rightC=0x0100,
-    
-    topL = 0x0000,
-    leftL=0x0001,
-    bottomL=0x0002,
-    rightL=0x0004,
-    
-    
-    inConor = topC|leftC|bottomC|rightC,
-    inLine = topL|leftL|bottomL|rightL
-} ;
-
-
-@class PointContext;
-
-CGRect rectInflate(CGPoint pt, CGFloat edge)
-{
-    return CGRectMake(pt.x - edge/2, pt.y - edge/2, edge, edge);
-}
-
-EPtState ptState(CGPoint pt, CGRect frame)
-{
-    if (rectInflate(frame.topleft) ) {
-        
-    }
-    return  topC;
-}
-
-
-bool isInCorner(EPtState e)
-{
-    return  (e | inConor) == inConor;
-}
-
-bool isInLine(EPtState e)
-{
-    return (e | inLine) == inLine;
-}
 
 
 
-@interface PointContext : NSObject
-@property (nonatomic) UIView *view;
-@property (nonatomic) enum EPtState ptCtx;
-@end
 
-@implementation PointContext
-@synthesize view, ptCtx;
-@end
-
-
-
-@interface ViewController ()
-@property (strong, nonatomic) IBOutlet UITextView *infoTextView;
-
-@end
-
-
-//find the deepest view
-//priority corner > line = block
-PointContext* hitTest(CGPoint pt,  UIView* root)
-{
-    PointContext * ctx = [[PointContext alloc] init];
-    ctx.view = root;
-    ctx.ptCtx = inside;
-    std::list<UIView*> q;
-    q.push_front(root);
-    
-    while (!q.empty()) {
-        UIView *top = q.back();
-        q.pop_back();
-        
-        for(UIView *childView in top.subviews) {
-            CGPoint pt = [childView convertPoint: pt fromView:root];
-            EPtState state = ptState(pt, childView.frame);
-            if (isInCorner( state )) {
-                ctx.ptCtx = state;
-                ctx.view = childView;
-            }
-            else if (isInLine(state) && !isInCorner(ctx.ptCtx)) {
-                ctx.ptCtx = state;
-                ctx.view = childView;
-            }
-            else if ( state == inside && !isInCorner(ctx.ptCtx)) {
-                ctx.ptCtx = state;
-                ctx.view = childView;
-            }
-            q.push_front(childView);
-        }
-    }
-    return ctx;
-}
 @end
